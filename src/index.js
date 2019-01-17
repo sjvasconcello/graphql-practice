f621adb02b8b8f5cc8e215e7465bc06e4eee3de1
sjvasconcello <sjvasconcello@gmail.com>
2019-01-17 19:26:49 -0300

    GraphQL Beginner Series Part-1

--- src/index.js
+++ src/index.js
@@ -1,18 +1,76 @@
-const { ApolloServer, gql } =  require('apollo-server');
+const {
+	ApolloServer,
+	gql
+} = require('apollo-server');
 
-const typeDefs = gql`
+const typeDefs = gql `
+	type Query {
+		hello: String!
+		user: User
+	}
 
-type Query {
-	hello: String!
-}
+	type User {
+		id: ID!
+		username: String!
+	}
+
+	type Error {
+		field: String!
+		message: String!
+	}
+
+	type RegisterResponse {
+		user: User
+		errors: [Error]
+	}
+
+	input UserInfo {
+		username: String!, 
+		password: String!, 
+		age: Int
+	}
+
+	type Mutation {
+		register(userInfo: UserInfo!): RegisterResponse!
+		login(userInfo: UserInfo!): Boolean!
+
+	}
 `;
 
 const resolvers = {
 	Query: {
-		hello: () => 'hello world'
+		hello: () => 'hello world', 
+		user: () => ({
+			id: 1,
+			username: "bob"
+		})
+	},
+	Mutation: {
+		login: () => true,
+		register: () => ({
+			errors: [
+			{
+				field: 'username1',
+				message: 'bad'
+			},
+			{
+				field: 'username2',
+				message: 'bad2'
+			}
+		],
+			user: {
+				id: 1,
+				username: "bob"
+			}
+		})
 	}
 }
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({
+	typeDefs,
+	resolvers
+})
 
-server.listen().then(({url}) => console.log(`server started at ${url}`));
+server.listen().then(({
+	url
+}) => console.log(`server started at ${url}`));
