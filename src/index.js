c52f9f08e864427b2d6af583c6bccad2678f842c
sjvasconcello <sjvasconcello@gmail.com>
2019-01-18 18:12:37 -0300

    How GraphQL Resolvers Work

--- src/index.js
+++ src/index.js
@@ -1,76 +1,87 @@
 const {
-	ApolloServer,
-	gql
+  ApolloServer,
+  gql,
 } = require('apollo-server');
 
-const typeDefs = gql `
-	type Query {
-		hello: String!
-		user: User
-	}
+const typeDefs = gql`
+  type Query {
+    hello(name: String): String!
+    user: User
+  }
 
-	type User {
-		id: ID!
-		username: String!
-	}
+  type User {
+    id: ID!
+    username: String!
+    firstLetterOrUsername: String!
+  }
 
-	type Error {
-		field: String!
-		message: String!
-	}
+  type Error {
+    field: String!
+    message: String!
+  }
 
-	type RegisterResponse {
-		user: User
-		errors: [Error]
-	}
+  type RegisterResponse {
+    user: User
+    errors: [Error]
+  }
 
-	input UserInfo {
-		username: String!, 
-		password: String!, 
-		age: Int
-	}
+  input UserInfo {
+    username: String!, 
+    password: String!, 
+    age: Int
+  }
 
-	type Mutation {
-		register(userInfo: UserInfo!): RegisterResponse!
-		login(userInfo: UserInfo!): Boolean!
-
-	}
+  type Mutation {
+    register(userInfo: UserInfo!): RegisterResponse!
+    login(userInfo: UserInfo!): String!
+}
 `;
 
 const resolvers = {
-	Query: {
-		hello: () => 'hello world', 
-		user: () => ({
-			id: 1,
-			username: "bob"
-		})
-	},
-	Mutation: {
-		login: () => true,
-		register: () => ({
-			errors: [
-			{
-				field: 'username1',
-				message: 'bad'
-			},
-			{
-				field: 'username2',
-				message: 'bad2'
-			}
-		],
-			user: {
-				id: 1,
-				username: "bob"
-			}
-		})
-	}
-}
+  User: {
+    // username: parent => parent.username,
+    firstLetterOrUsername: parent => parent.username[0],
+  },
+  Query: {
+    hello: (parent, {
+      name,
+    }) => `Hello! ${name}`,
+    user: () => ({
+      id: 3,
+      username: 'Michael',
+    }),
+  },
+  Mutation: {
+    login: async (parent, { userInfo: { username } }, context, info) => {
+      // Check the password
+      // await checkPassword(password)
+      return username;
+    },
+    register: () => ({
+      errors: [{
+        field: 'username1',
+        message: 'bad',
+      },
+      {
+        field: 'username2',
+        message: 'bad2',
+      },
+      ],
+      user: {
+        id: 1,
+        username: 'Michael',
+      },
+    }),
+  },
+};
 
 const server = new ApolloServer({
-	typeDefs,
-	resolvers
-})
+  typeDefs,
+  resolvers,
+  context: ({ req, res }) => ({ req, res }),
+});
 
 server.listen().then(({
-	url
+  url,
+// eslint-disable-next-line no-console
 }) => console.log(`server started at ${url}`));
